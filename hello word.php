ec0d93cd75927e288ab10ce042a6706f0e1f7b66
StassyB <100481391+StassyB@users.noreply.github.com>
2022-03-28 09:00:31 +0300

    Create hello word.php

---
+++ hello word.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+echo "<h1>Am Learning</h1>";
+echo "<hr>";
+echo "Hello Guys,";
+echo" <p> This is my site </p>";
+?>
+  </body>
+</html>
