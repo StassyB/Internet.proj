064b9c0e6e7e7762689f3d96e32032d826658f89
StassyB <100481391+StassyB@users.noreply.github.com>
2022-03-28 12:48:41 +0300

    Create variales.php

---
+++ variales.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    echo "<h1> NAME AND NUMBER VARIABLES</H1>";
+    echo "<hr>";
+    $CharacterName = "john";
+    $CharacterScore = "100%" ;
+    $CharacterSubject ="Math";
+    echo "I have a classmet named $CharacterName <br>";
+  echo "$CharacterName loves $CharacterSubject<br>";
+  echo "And scored $CharacterScore <br>";
+    echo "<hr>";
+     ?>
+  </body>
+</html>
